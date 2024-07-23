4fdd8d3778909e805339f882689ed3f449af1bc3
IDOMATH <btdothemath@gmail.com>
2024-07-23 16:31:50 -0500

    Adding some logic

--- Renderer/renderer.go
+++ Renderer/renderer.go
@@ -6,21 +6,46 @@ import (
 )
 
 type Renderer struct {
-	viewsLocation    string
-	partialsLocation string
-	persistCache     bool
-	TemplateCache    map[string]*template.Template
+	viewsLocation         string
+	viewsFileExtension    string
+	partialsLocation      string
+	partialsFileExtension string
+	persistCache          bool
+	TemplateCache         map[string]*template.Template
 }
 
-func NewRenderer(viewsLocation, partialsLocation string, persistCache bool) *Renderer {
-	return &Renderer{
-		viewsLocation:    viewsLocation,
-		partialsLocation: partialsLocation,
-		persistCache:     persistCache,
-		TemplateCache:    make(map[string]*template.Template),
+func NewRenderer(viewsLocation, viewsFileExtension, partialsFileExtension, partialsLocation string, persistCache bool) *Renderer {
+
+	rr := &Renderer{
+		viewsLocation:         viewsLocation,
+		viewsFileExtension:    viewsFileExtension,
+		partialsLocation:      partialsLocation,
+		partialsFileExtension: partialsFileExtension,
+		persistCache:          persistCache,
 	}
+	tc, err := rr.CreateTemplateCache()
+	if err != nil {
+		panic("could not CreateTemplateCache in NewRenderer")
+	}
+	rr.TemplateCache = tc
+	return rr
 }
 
-func Render(w http.ResponseWriter, r *http.Request, name string) {
+func (rr *Renderer) Render(w http.ResponseWriter, r *http.Request, name string) error {
 
+	if !rr.persistCache {
+		tc, err := rr.CreateTemplateCache()
+		if err != nil {
+			return err
+		}
+		rr.TemplateCache = tc
+	}
+
+	return nil
+}
+
+func (rr *Renderer) CreateTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	return cache, nil
 }
