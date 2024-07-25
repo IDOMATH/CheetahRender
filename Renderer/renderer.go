e4ef795758bebdbc3270d554b981c44f8d173709
IDOMATH <btdothemath@gmail.com>
2024-07-24 20:10:44 -0500

    Adding CreateTemplateCache logic

--- Renderer/renderer.go
+++ Renderer/renderer.go
@@ -1,8 +1,10 @@
 package renderer
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
 type Renderer struct {
@@ -23,29 +25,53 @@ func NewRenderer(viewsLocation, viewsFileExtension, partialsFileExtension, parti
 		partialsFileExtension: partialsFileExtension,
 		persistCache:          persistCache,
 	}
-	tc, err := rr.CreateTemplateCache()
-	if err != nil {
-		panic("could not CreateTemplateCache in NewRenderer")
-	}
-	rr.TemplateCache = tc
+	rr.CreateTemplateCache()
 	return rr
 }
 
 func (rr *Renderer) Render(w http.ResponseWriter, r *http.Request, name string) error {
 
 	if !rr.persistCache {
-		tc, err := rr.CreateTemplateCache()
-		if err != nil {
-			return err
-		}
-		rr.TemplateCache = tc
+		rr.CreateTemplateCache()
 	}
 
 	return nil
 }
 
-func (rr *Renderer) CreateTemplateCache() (map[string]*template.Template, error) {
+func (rr *Renderer) CreateTemplateCache() {
 	cache := map[string]*template.Template{}
 
-	return cache, nil
+	pages, err := filepath.Glob(fmt.Sprintf("%s/*.%s", rr.viewsLocation, rr.viewsFileExtension))
+	if err != nil {
+		fmt.Println("could not find views: ", err)
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			fmt.Println(err.Error())
+			rr.TemplateCache = cache
+			return
+		}
+
+		matches, err := filepath.Glob(fmt.Sprintf("%s/*.%s", rr.partialsLocation, rr.partialsFileExtension))
+		if err != nil {
+			fmt.Println(err.Error())
+			rr.TemplateCache = cache
+			return
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob(fmt.Sprintf("%s/*.%s", rr.partialsLocation, rr.partialsFileExtension))
+			if err != nil {
+				fmt.Println(err.Error())
+				rr.TemplateCache = cache
+				return
+			}
+		}
+		cache[name] = ts
+	}
+
+	rr.TemplateCache = cache
 }
